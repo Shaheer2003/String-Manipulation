b1655dc38738e29fcd492424a1e247a9651fd842
Shaheer2003 <55394184+Shaheer2003@users.noreply.github.com>
2019-10-03 19:26:49 +0500

    Merging two strings of two words
    This VB source code takes the 1st word of one string and 2nd word of another string and merges them

---
+++ Merging two words.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Str1, Str2, Str3, FirstWord, LastWord As String
+        Dim Space As Integer
+
+        Str1 = ""
+        Str2 = ""
+        Str3 = ""
+        FirstWord = ""
+        LastWord = ""
+        Space = 0
+
+        Console.Write("Enter 1st String of 2 Words: ")
+        Str1 = Console.ReadLine
+
+        Console.Write("Enter 2nd string of 2 words: ")
+        Str2 = Console.ReadLine
+
+        Space = InStr(Str1, " ")
+        FirstWord = Left(Str1, Space - 1)
+
+        Space = InStr(Str2, " ")
+        LastWord = Right(Str2, Len(Str2) - Space)
+
+        Str3 = FirstWord & " " & LastWord
+        Console.WriteLine(Str3)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
